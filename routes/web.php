193d85da502ad4047c4483ca4198089b0f5b3486
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-04-06 21:34:08 +0300

    Routes Topic Is Ready

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Static Route
+
+Route::get('/','ThemeController@home_page');
+
+
+// Daynamic Route
+
+// Route::get('/user/{name}',function ($name){
+//     echo $name;
+// })->where('name','[A-Za-z]+'); 
+
+
+// Route::get('/user/{id}',function ($id){
+//     echo $id;
+// })->where('id','[0-9]+');
+
+// Route::get('/user/student/course/attend/register',function (){
+//     echo 'welcome user regsiteration';
+// })->name('students');
+
+
+// Route::prefix('dashboard')->middleware('auth')->group(function (){
+
+//     Route::get('/main',function (){
+//         echo 'welcome admin';
+//     });
+
+// });
